8c12e53c457a1d4da6ccecb8702fd1407cd5eda1
Pruthvirajsinh Zala <prithvirajsinhzala99@gmail.com>
2025-07-01 14:50:21 +0530

    code added

--- recommend.py
+++ recommend.py
@@ -15,3 +15,15 @@ logging.info("Loading Data...")
 try:
     df = joblib.load('df_cleaned.pkl')
     cosine_sim = joblib.load('cosine_sim.pkl')
+    logging.info("Data loaded  successfully.")
+except Exception as e:
+    logging.error("Failed to load requested files : %s", str(e))
+    raise e
+
+def recommend_movie(movie_name, top_n=5):
+    logging.info("Reccomending movies for : '%s'", movie_name)
+    idx = df[df['title'].str.lower() == movie_name.lower()].index
+    if len(idx) == 0:
+        logging.warning("Movie Not Found in Database.")
+        return None
+    idx = idx[0]
