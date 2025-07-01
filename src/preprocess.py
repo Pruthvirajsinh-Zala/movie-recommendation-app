00acb7e6449f2f3de4f8a8ed6c4f0dc285b1f853
Pruthvirajsinh Zala <prithvirajsinhzala99@gmail.com>
2025-07-01 15:35:06 +0530

    code update

--- src/preprocess.py
+++ src/preprocess.py
@@ -8,3 +8,19 @@ from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+#setup logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("preprocess.log", encoding="utf-8"),
+        logging.StreamHandler()
+    ]
+)
+
+logging.info("Starting preprocessing...")
+
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
