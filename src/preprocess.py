447de06e98225b9773ff9abccb5cd6335fcaf3d1
Pruthvirajsinh Zala <prithvirajsinhzala99@gmail.com>
2025-07-01 15:32:49 +0530

    preprocess update

---
+++ src/preprocess.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import re
+import nltk
+import joblib 
+import logging
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
